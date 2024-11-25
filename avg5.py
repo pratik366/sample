415bdf3d3353f1d6835f667e3f1d66d94f1aecb3
pratik366 <pratikpaditkar2023@gmail.com>
2024-11-25 20:04:08 +0530

    Create avg5.py

---
+++ avg5.py
@@ -0,0 +1,12 @@
+# Input 5 numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+num4 = float(input("Enter fourth number: "))
+num5 = float(input("Enter fifth number: "))
+
+# Calculate the average
+average = (num1 + num2 + num3 + num4 + num5) / 5
+
+# Print the average
+print("The average of the 5 numbers is:", average)
